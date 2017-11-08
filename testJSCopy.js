367b802d79d57d07ae1e41573d3d4ab3f61db607
sarulse <sarulselv@gmail.com>
2017-11-08 16:58:33 -0500

    Formatted

--- testJSCopy.js
+++ testJSCopy.js
@@ -1,34 +1,35 @@
-"use strict";   
+"use strict";
 
-var Testing = {        
-        clickedList : [],
-        pushIfDoesntExist : function (arr, item){    
-            if(arr.indexOf(item) === -1) {
-                arr.push(item);
-                return true;
-            }
-            return false;
-        },
-        applyListeners: function() {
-            //converting HTML collection into an Array
-            var nodes = Array.from(document.getElementsByTagName("li"));                    
-            var self = this;
-            //used forEach to replace "for" for readability and performance               
-            nodes.forEach(function(node){
-                node.addEventListener("click", function() {
-                    self.checkItemExists(self, this.textContent);
-                }, false);                   
-            });                    
-            
-        },
-        //checks if Item exists in the array and display data
-        checkItemExists: function(testObj, arrElem){                                   
-            testObj.pushIfDoesntExist(this.clickedList, arrElem);
-            testObj.displayData();                    
-        },
-        displayData: function() {
-            var textBox = document.getElementById("clickedElements");
-            textBox.textContent = this.clickedList.join(', ');
+var Testing = {
+    clickedList: [],
+    //Added "pushIfDoesntExist" function to avoid extending array.prototype
+    pushIfDoesntExist: function (arr, item) {
+        if (arr.indexOf(item) === -1) {
+            arr.push(item);
+            return true;
         }
+        return false;
+    },
+    applyListeners: function () {
+        //converting HTML collection into an Array
+        var nodes = Array.from(document.getElementsByTagName("li"));
+        var self = this;
+        //used forEach to replace "for" for readability and performance               
+        nodes.forEach(function (node) {
+            node.addEventListener("click", function () {
+                self.checkItemExists(self, this.textContent);
+            }, false);
+        });
+
+    },
+    //checks if Item exists in the array and display data
+    checkItemExists: function (testObj, arrElem) {
+        testObj.pushIfDoesntExist(this.clickedList, arrElem);
+        testObj.displayData();
+    },
+    displayData: function () {
+        var textBox = document.getElementById("clickedElements");
+        textBox.textContent = this.clickedList.join(", ");
+    }
 };
 Testing.applyListeners();
